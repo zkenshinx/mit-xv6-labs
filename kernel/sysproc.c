808f0f348c5bf71022ba9c1588fc8752d0905cc7
Koba Karaputadze <kobakaraputadze701@gmail.com>
2024-05-15 11:15:16 +0400

    finish lab pgtbl

---
+++ kernel/sysproc.c
@@ -0,0 +1,121 @@
+#include "types.h"
+#include "riscv.h"
+#include "param.h"
+#include "defs.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+uint64
+sys_exit(void)
+{
+  int n;
+  argint(0, &n);
+  exit(n);
+  return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return fork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  argaddr(0, &p);
+  return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  uint64 addr;
+  int n;
+
+  argint(0, &n);
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+
+  argint(0, &n);
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(killed(myproc())){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+
+#ifdef LAB_PGTBL
+#define PG_ACCESS_MAX 64
+int
+sys_pgaccess(void)
+{
+  // lab pgtbl: your code here.
+  char result_buf[PG_ACCESS_MAX >> 3];
+  uint64 va, user_buf;
+  int npage;
+  argaddr(0, &va);
+  argint(1, &npage);
+  argaddr(2, &user_buf);
+
+  memset(result_buf, 0, sizeof(result_buf));
+  pagetable_t pagetable = myproc()->pagetable;
+  for (int i = 0; i < npage; ++i) {
+    pte_t *pte = walk(pagetable, va + i * PGSIZE, 0);
+    if (*pte & PTE_A) {
+      result_buf[i >> 3] |= (1 << (i & 7));
+      *pte &= ~PTE_A;
+    }
+  }
+  if (copyout(pagetable, user_buf, result_buf, npage >> 3) < 0)
+    return -1;
+  return 0;
+}
+#endif
+
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  argint(0, &pid);
+  return kill(pid);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
